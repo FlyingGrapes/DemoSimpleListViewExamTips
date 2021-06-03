66a76be80a88f29ef2080f72f2e585e2592ae4d3
20018621 <gracengxy2021@gmail.com>
2021-06-03 18:30:55 +0800

    Worksheet submission

---
+++ app/src/main/java/sg/edu/rp/c346/id20018621/demosimplelistviewexamtips/MainActivity.java
@@ -0,0 +1,48 @@
+package sg.edu.rp.c346.id20018621.demosimplelistviewexamtips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvExamTips;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //call
+        lvExamTips = findViewById(R.id.listViewExamTips);
+
+        //create array
+        String [] examTipsArray;
+
+        examTipsArray = new String[5];
+        examTipsArray[0] = "Don't just read the code, code it as much as possible during each practical session";
+        examTipsArray[1] = "Seek help from the lecturer ASAP, don't wait till you lost in the jungle";
+        examTipsArray[2] = "Prepare your template source code for each topic";
+        examTipsArray[3] = "Create a few empty Android projects to speed up your coding during the exam";
+        examTipsArray[4] = "Ensure that your Android Studio is up and running before the exam";
+
+        //connect lvExamTips, ListView, examTipsArray
+        //display examTipsArray in list, print the adapter
+        ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, examTipsArray);
+        lvExamTips.setAdapter(adapter);
+
+        lvExamTips.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String msg = examTipsArray[position];
+                Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
